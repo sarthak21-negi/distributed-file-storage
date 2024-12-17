30fe85bfb3e7ea3cd2c1b4963675746547c8d4c5
sarthak21-negi <singhsarthak378@gmail.com>
2024-12-17 19:55:16 +0530

    storage hash

--- storage.go
+++ storage.go
@@ -1,13 +1,37 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
 	"io"
 	"log"
 	"os"
+	"strings"
 )
+type PathKey struct{
+	PathName string
+	Original string
+} 
 
+func CASPathTransformFunc(key string) PathKey{
+	hash := sha1.Sum([]byte(key))
+	hashStr := hex.EncodeToString(hash[:])
 
-type PathTransformFunc func(string) string
+	blocksize := 5
+	sliceLen := len(hashStr) / blocksize
+	paths := make([]string, sliceLen)
+
+	for i := 0; i < sliceLen; i++ {
+		from, to := i*blocksize, (i*blocksize)+blocksize
+		paths[i] = hashStr[from:to]
+	}
+	return PathKey{
+		PathName: strings.Join(paths, "/"),
+		Original: hashStr,
+	}
+}
+type PathTransformFunc func(string) PathKey
 
 type StoreOpts struct{
 	PathTransformFunc PathTransformFunc
@@ -26,14 +50,17 @@ func NewStore(opts StoreOpts) *Store {
 	}
 }
 
+func (p PathKey) Filename() string{
+	return fmt.Sprintf ("%s/%s", p.PathName, p.Original)
+}
+
 func(s *Store) writeStream(key string, r io.Reader) error{
-	pathName := s.PathTransformFunc(key)
-	if err := os.MkdirAll(pathName, os.ModePerm); err != nil {
+	pathKey := s.PathTransformFunc(key)
+	if err := os.MkdirAll(pathKey.PathName, os.ModePerm); err != nil {
 		return err
 	}
 
-	fileName := "filename"
-	pathAndFileName := pathName+ "/" + fileName
+	pathAndFileName := pathKey.Filename()
 
 	f,err := os.Create(pathAndFileName)
 	if err != nil{
